2eea927b0658916ac8527ec2b57d810b1fd7a6f3
Pedrito16 <proibidofantasma@gmail.com>
2025-07-09 00:28:04 -0300

    fiz um efeitozinho quando cresce
    Ideia: fazer uma terrinha nos sprite pra esconder os quadrados

--- prantation/Assets/Scripts/WeedScript.cs
+++ prantation/Assets/Scripts/WeedScript.cs
@@ -4,24 +4,76 @@ using UnityEngine;
 public class WeedScript : MonoBehaviour
 {
     [SerializeField] float timeToGrow = 0.5f; // Time in minutes for the weed to grow
-    float timeElapsed = 0f;
+    [SerializeField] Sprite[] spritesToChange = new Sprite[4];
+    [Tooltip("Os sprites para diferentes tipos de tamanho (0%,25%, 50%, 100%")]
+    SpriteRenderer spriteRenderer;
+    float lastGrowth;
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = spritesToChange[0];
+        StartCoroutine(GrowEnumerator());
     }
     void Update()
     {
         
     }
-    public IEnumerator GrowEnumerator()
+    private IEnumerator GrowEnumerator()
     {
         float timeInSeconds = timeToGrow * 60f;
         float iterador = 0f;
-        while(iterador < 1)
+        while(iterador < timeInSeconds)
         {
             iterador += Time.deltaTime;
+            CheckPercent(timeInSeconds, iterador);
+            print(iterador);
             yield return null;
         }
-
+    }
+    public IEnumerator Bobbing(float duration)
+    {
+        float iterador = 0;
+        float dividedDuration = duration / 2;
+        Vector2 initialPos = transform.localScale;
+        Vector2 newPos = new Vector2(initialPos.x + 0.1f, initialPos.y + 0.1f);
+        while (iterador < dividedDuration)
+        {
+            iterador += Time.deltaTime;
+            transform.localScale = Vector2.Lerp(transform.localScale, newPos, iterador / dividedDuration);
+            yield return null;
+        }
+        print("SEgunda parte");
+        iterador = 0;
+        while(iterador < dividedDuration)
+        {
+            iterador += Time.deltaTime;
+            transform.localScale = Vector2.Lerp(transform.localScale, initialPos, iterador / dividedDuration);
+            yield return null;
+        }
+    }
+    void CheckPercent(float numberToDiscover, float numberToApplyPercent)
+    {
+        float percent = (numberToApplyPercent / numberToDiscover) * 100;
+        if (percent < 25f) return;
+        else if (percent >= 25f && percent < 50f && lastGrowth == 0)
+        {
+            lastGrowth += 1;
+            spriteRenderer.sprite = spritesToChange[1];
+            StartCoroutine(Bobbing(0.5f));
+        }
+        else if(percent >= 50f && percent < 75f && lastGrowth == 1)
+        {
+            lastGrowth += 1;
+            spriteRenderer.sprite = spritesToChange[2];
+            StartCoroutine(Bobbing(0.5f));
+            return;
+        }
+        else if(percent >= 100f && lastGrowth == 2)
+        {
+            lastGrowth += 1;
+            spriteRenderer.sprite = spritesToChange[3];
+            StartCoroutine(Bobbing(0.5f));
+            return;
+        }
     }
 }
